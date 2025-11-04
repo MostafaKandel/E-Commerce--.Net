ea356571ae1246eec304a34c0542642aec107770
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2025-11-04 20:31:58 +0100

    feat: implementation of Repository and unit of work

---
+++ E Commerce.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using E_Commerce.Domain.Contracts;
+using E_Commerce.Domain.Entities;
+using E_Commerce.Persistence.Data.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly StoreDbContext _dbContext;
+        private readonly Dictionary<Type, object> repositories = [];
+
+        public UnitOfWork(StoreDbContext dbContext) {
+            _dbContext = dbContext;
+        }
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+           var EntityType = typeof(TEntity);
+            if (repositories.TryGetValue(EntityType, out object? repository))
+                return (IGenericRepository<TEntity, TKey>)repository;
+            var NewRepo= new GenericRepository<TEntity, TKey>(_dbContext);
+            repositories[EntityType]= NewRepo;
+            return NewRepo;
+
+        }
+
+        public async Task<int> SaveChangesAsync()=> await _dbContext.SaveChangesAsync();
+
+    }
+}
